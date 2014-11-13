cdc260bb977d7b15e9e2870c1cda24e0a72d9cc9
Roger Nordquist <roger.nordquist@valtech.se>
2014-11-13 17:01:44 +0100

    making build work

--- src/main/groovy/org/rogern/gradlefmpp/FmppPlugin.groovy
+++ src/main/groovy/org/rogern/gradlefmpp/FmppPlugin.groovy
@@ -8,9 +8,7 @@ class FmppPlugin implements Plugin<Project> {
   @Override
   void apply(Project project) {
     project.extensions.create("fmpp", FmppPluginExtension)
-    project.configurations << {
-        fmpp
-    }
+
     project.task('fmpp') << {
       ant.taskdef(name: 'fmpp', classname: 'fmpp.tools.AntTask', classpath: configurations.fmpp.asPath)
       def props = project.extensions.fmpp
