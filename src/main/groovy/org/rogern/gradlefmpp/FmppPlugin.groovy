03fef0b8a839574747a5aac153b4c320ee11e485
Roger Nordquist <roger.nordquist@valtech.se>
2014-11-13 16:49:52 +0100

    restructure and id change

---
+++ src/main/groovy/org/rogern/gradlefmpp/FmppPlugin.groovy
@@ -0,0 +1,21 @@
+package org.rogern.gradlefmpp
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class FmppPlugin implements Plugin<Project> {
+
+  @Override
+  void apply(Project project) {
+    project.extensions.create("fmpp", FmppPluginExtension)
+    project.configurations << {
+        fmpp
+    }
+    project.task('fmpp') << {
+      ant.taskdef(name: 'fmpp', classname: 'fmpp.tools.AntTask', classpath: configurations.fmpp.asPath)
+      def props = project.extensions.fmpp
+      ant.fmpp(sourceRoot: props.fmppSourceRoot, outputRoot: props.fmppOutputDir, includes: props.fmppIncludes,
+          replaceExtension: props.fmppReplaceExtension, data: props.fmppData)
+    }
+  }
+}
---
+++ src/main/groovy/org/rogern/gradlefmpp/FmppPluginExtension.groovy
@@ -0,0 +1,9 @@
+package org.rogern.gradlefmpp
+
+class FmppPluginExtension {
+  String fmppSourceRoot
+  String fmppOutputDir
+  String fmppIncludes
+  String fmppReplaceExtension
+  String fmppData
+}
