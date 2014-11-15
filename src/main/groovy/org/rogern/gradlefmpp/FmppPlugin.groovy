917e8331a335b114bae1d2b2757f81f088a248ee
Roger Nordquist <roger.nordquist@valtech.se>
2014-11-15 13:43:19 +0100

    renamed plugin

--- src/main/groovy/org/rogern/gradlefmpp/FmppPlugin.groovy
+++ src/main/groovy/org/rogern/gradlefmpp/FmppPlugin.groovy
@@ -7,12 +7,12 @@ class FmppPlugin implements Plugin<Project> {
 
   @Override
   void apply(Project project) {
-    project.extensions.create("fmpp", FmppPluginExtension)
+    project.extensions.create("gradlefmppargs", FmppPluginExtension)
 
     project.task('fmpp') << {
-      ant.taskdef(name: 'fmpp', classname: 'fmpp.tools.AntTask', classpath: configurations.fmpp.asPath)
-      def props = project.extensions.fmpp
-      ant.fmpp(sourceRoot: props.fmppSourceRoot, outputRoot: props.fmppOutputDir, includes: props.fmppIncludes,
+      project.ant.taskdef(name: 'fmpp', classname: 'fmpp.tools.AntTask', classpath: project.buildscript.configurations.classpath.asPath)
+      def props = project.extensions.gradlefmppargs
+      project.ant.fmpp(sourceRoot: props.fmppSourceRoot, outputRoot: props.fmppOutputDir, includes: props.fmppIncludes,
           replaceExtension: props.fmppReplaceExtension, data: props.fmppData)
     }
   }
